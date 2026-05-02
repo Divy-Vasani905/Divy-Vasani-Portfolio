55638ee07b0e6bb4904a1a98fea39d65d404348c
Divy Vasani <divyvasani09@gmail.com>
2026-05-02 19:30:46 +0530

    Fix Vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,4 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/Divy-Vasani-Portfolio",
 });
-
-// import { defineConfig } from 'vite'
-
-// import tailwindcss from "@tailwindcss/vite";
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react(),tailwindcss()],
-//   server: {
-//     host: '0.0.0.0', // Allows external access
-//     port: 5173,
-//     strictPort: true,
-//     allowedHosts: [
-//       'e58b-110-227-210-237.ngrok-free.app', // Add your Ngrok host manually
-//       '.ngrok-free.app' // Allow all Ngrok URLs
-//     ],
-//     cors: {
-//       origin: '*',
-//       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//       allowedHeaders: ['Content-Type', 'Authorization'],
-//     },
-//   },
-// });
-
-
