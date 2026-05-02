6c7cd9abae08fdf622bf6691a395c4162eccb196
Divy Vasani <divyvasani09@gmail.com>
2026-05-02 12:56:20 +0530

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [tailwindcss()],
-  base:"/My-Portfolio",
+  plugins: [react(), tailwindcss()],
+  base:"/Divy-Vasani-Portfolio",
 });
 
 // import { defineConfig } from 'vite'
